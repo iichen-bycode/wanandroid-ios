206bf9cc38fa84c3795d79c2a7aaf8f1462af771
iichen <iichen>
2025-06-13 13:47:32 +0800

    Initial Commit

---
+++ wanandroid/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wanandroid
+//
+//  Created by gaimi on 2025/6/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world2     !")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wanandroid/wanandroidApp.swift
@@ -0,0 +1,17 @@
+//
+//  wanandroidApp.swift
+//  wanandroid
+//
+//  Created by gaimi on 2025/6/13.
+//
+
+import SwiftUI
+
+@main
+struct wanandroidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
