0c40a25d2321b22833d210d726fce6376dc538bb
iichen <iichen>
2025-06-17 18:20:01 +0800

    侧边栏登录状态等

---
+++ wanandroid/SlideOutMenu.swift
@@ -0,0 +1,18 @@
+//
+//  SlideOutMenu.swift
+//  wanandroid
+//
+//  Created by gaimi on 2025/6/17.
+//
+
+import SwiftUI
+
+struct SlideOutMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SlideOutMenu()
+}
