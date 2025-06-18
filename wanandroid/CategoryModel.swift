ab67b1eea8d4451d84f65075d8b1edb594d7e7a9
iichen <iichen>
2025-06-18 17:14:35 +0800

    主模块

---
+++ wanandroid/CategoryModel.swift
@@ -0,0 +1,15 @@
+//
+//  WechatAuthorModel.swift
+//  wanandroid
+//
+//  Created by gaimi on 2025/6/18.
+//
+
+import Foundation
+struct CategoryModel: Codable {
+    let id: Int
+    let name: String
+    let userControlSetTop: Bool
+    let visible: Int
+
+}
---
+++ wanandroid/ProjectViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProjectViewModel.swift
+//  wanandroid
+//
+//  Created by gaimi on 2025/6/18.
+//
+
+import Foundation
